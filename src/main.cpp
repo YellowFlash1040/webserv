fbd0623c853ac758153212805392bb2c8e4a8a50
akovtune <akovtune@student.codam.nl>
2025-09-04 12:23:11 +0200

    Add to the previous commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
-#include "network.hpp"
+#include "Server.hpp"
+#include <iostream>
+#include <string.h>
+#include <csignal>
+
+volatile bool g_running = true;
+
+void handle_signal(int)
+{
+    g_running = false;
+}
 
 int main(void)
 {
-    int serverSocket = init(8080);
-    run(serverSocket);
+    std::signal(SIGINT, handle_signal);
+    std::signal(SIGTERM, handle_signal);
+
+    try
+    {
+        Server s(8080);
+        (void)s;
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cerr << "Error: " << e.what() << ": " << strerror(errno) << "\n";
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << "\n";
+    }
 }
