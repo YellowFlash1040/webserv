cc5c9b7483d866eba3ba1ac5d930b911767a29c0
akovtune <akovtune@student.codam.nl>
2025-09-04 12:24:20 +0200

    Add to the previous 2 commits

--- src/network/Server/Server.cpp
+++ src/network/Server/Server.cpp
@@ -76,6 +76,21 @@ void Server::run(void)
     while (true)
     {
         // server life cycle loop code
+        int readyFDs = epoll_wait(epfd, FDs, MAX_EVENTS, -1);
+        if (readyFDs == -1)
+        {
+            if (errno == EINTR)
+                continue; // interrupted by signal, retry
+            perror("epoll_wait");
+            break;
+        }
+        for (int i = 0; i < readyFDs; ++i)
+        {
+            if (FDs[i].data.fd == serverSocket)
+                result = acceptNewClient(epfd, serverSocket);
+            else
+                processClient(FDs[i].data.fd);
+        }
     }
 }
 
