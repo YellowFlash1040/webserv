6d758e017f68a811d30f512b51e64f85572e5dbc
akovtune <akovtune@student.codam.nl>
2025-11-04 17:11:24 +0100

    Created NetworkInteface and NetworkEnpoint classes

---
+++ src/network/NetworkEndpoint/NetworkEndpoint.cpp
@@ -0,0 +1,61 @@
+#include "NetworkEndpoint.hpp"
+
+// -----------------------CONSTRUCTION AND DESTRUCTION-------------------------
+
+// Default constructor
+NetworkEndpoint::NetworkEndpoint(NetworkInterface interface, int port)
+  : m_interface(interface)
+  , m_port(port)
+{
+}
+
+// Copy constructor
+NetworkEndpoint::NetworkEndpoint(const NetworkEndpoint& other)
+  : m_interface(other.m_interface)
+  , m_port(other.m_port)
+{
+}
+
+// Copy assignment operator
+NetworkEndpoint& NetworkEndpoint::operator=(const NetworkEndpoint& other)
+{
+    if (this != &other)
+    {
+        m_interface = other.m_interface;
+        m_port = other.m_port;
+    }
+    return (*this);
+}
+
+// Move constructor
+NetworkEndpoint::NetworkEndpoint(NetworkEndpoint&& other) noexcept
+  : m_interface(other.m_interface)
+  , m_port(other.m_port)
+{
+}
+
+// Move assignment operator
+NetworkEndpoint& NetworkEndpoint::operator=(NetworkEndpoint&& other) noexcept
+{
+    if (this != &other)
+    {
+        m_interface = other.m_interface;
+        m_port = other.m_port;
+    }
+    return (*this);
+}
+
+// Destructor
+NetworkEndpoint::~NetworkEndpoint() {}
+
+// ---------------------------ACCESSORS-----------------------------
+
+NetworkInterface NetworkEndpoint::ip(void) const
+{
+    return (m_interface);
+}
+
+int NetworkEndpoint::port(void) const
+{
+    return (m_port);
+}
