9400ae3c4e7a9b7523dddae4308604448b5e2aa9
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-11-15 20:17:24 +0100

    refactor: marked Config's public member functions as const

--- src/config/Config/Config.hpp
+++ src/config/Config/Config.hpp
@@ -38,10 +38,10 @@ class Config
   public:
     // Methods
     static Config fromFile(const std::string& filepath);
-    std::vector<NetworkEndpoint> getAllEnpoints();
+    std::vector<NetworkEndpoint> getAllEnpoints() const;
     RequestContext createRequestContext(const NetworkEndpoint& endpoint,
                                         const std::string& host,
-                                        const std::string& uri);
+                                        const std::string& uri) const;
 
   private:
     // Properties
