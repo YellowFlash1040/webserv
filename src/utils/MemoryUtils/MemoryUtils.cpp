f19acfb7ca6ba8d2400b20cfc1e59105aa2e345f
akovtune <akovtune@student.codam.nl>
2025-11-22 17:55:52 +0100

    refactor(src): cleaned up some of the code

--- src/utils/MemoryUtils/MemoryUtils.cpp
+++ src/utils/MemoryUtils/MemoryUtils.cpp
@@ -11,15 +11,6 @@ void* memset(void* ptr, int value, std::size_t num)
     return (ptr);
 }
 
-void* memcpy(void* dest, const void* src, std::size_t num)
-{
-    unsigned char* d = static_cast<unsigned char*>(dest);
-    const unsigned char* s = static_cast<const unsigned char*>(src);
-    for (std::size_t i = 0; i < num; ++i)
-        d[i] = s[i];
-    return (dest);
-}
-
 void bzero(void* ptr, std::size_t num)
 {
     memset(ptr, 0, num);
