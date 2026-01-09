a15a7ebe440f249427de5ab203f40b84fbf2a04b
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-01-09 11:50:28 +0100

    feat(HttpMethod): extended the list of recognisable http methods

--- src/http/HttpMethod/HttpMethod.cpp
+++ src/http/HttpMethod/HttpMethod.cpp
@@ -10,6 +10,18 @@ std::string httpMethodToString(HttpMethod method)
         return "POST";
     case HttpMethod::DELETE:
         return "DELETE";
+    case HttpMethod::HEAD:
+        return "HEAD";
+    case HttpMethod::PUT:
+        return "PUT";
+    case HttpMethod::CONNECT:
+        return "CONNECT";
+    case HttpMethod::OPTIONS:
+        return "OPTIONS";
+    case HttpMethod::TRACE:
+        return "TRACE";
+    case HttpMethod::PATCH:
+        return "PATCH";
     default:
         return "UNKNOWN";
     }
@@ -23,5 +35,17 @@ HttpMethod stringToHttpMethod(const std::string& method)
         return HttpMethod::POST;
     if (method == "DELETE")
         return HttpMethod::DELETE;
+    if (method == "HEAD")
+        return HttpMethod::HEAD;
+    if (method == "PUT")
+        return HttpMethod::PUT;
+    if (method == "CONNECT")
+        return HttpMethod::CONNECT;
+    if (method == "OPTIONS")
+        return HttpMethod::OPTIONS;
+    if (method == "TRACE")
+        return HttpMethod::TRACE;
+    if (method == "PATCH")
+        return HttpMethod::PATCH;
     return HttpMethod::NONE;
 }
