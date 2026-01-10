f4f2ec42abeac4faf004f9e3de8f99158c78d911
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-01-10 13:18:35 +0100

    refactor(CGIManager): split one large function into several smaller ones

--- src/cgi/CGIManager.hpp
+++ src/cgi/CGIManager.hpp
@@ -24,6 +24,21 @@ class CGIManager
     // Methods
     static std::vector<std::string> buildEnvFromRequest(
         const RequestData& req, Client& client, const std::string& scriptPath);
+    static void addEnv(std::vector<std::string>& env, const std::string& key,
+                       const std::string& value);
+    static void addRequestInfo(std::vector<std::string>& env,
+                               const Client& client, const RequestData& req,
+                               const std::string& scriptPath);
+    static void addReqHeaders(std::vector<std::string>& env,
+                              const RequestData& req);
+    static void addServerInfo(std::vector<std::string>& env,
+                              const RequestData& req, const Client& client);
+    static void addServerName(std::vector<std::string>& env,
+                              const RequestData& req, const Client& client);
+    static void addServerAddr(std::vector<std::string>& env,
+                              const Client& client);
+    static void addRemoteAddr(std::vector<std::string>& env,
+                              const Client& client);
 };
 
 #endif
